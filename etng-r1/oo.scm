1d9e18d155a18ba9d58562fe1f0e4a836dbb3989
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2007-01-19 13:44:38 +1300

    More notes

--- etng-r1/oo.scm
+++ etng-r1/oo.scm
@@ -10,3 +10,23 @@
 ;;  - functions (objects that don't have a self)
 ;;  - promises (?)
 
+;; Patterns can be:
+;;  - discard
+;;  - messages (?)
+;;  - bindings
+;;  - tuples
+;;  - literals, ie. words, qnames, byte-vectors (?)
+
+;; Objects and functions differ only in whether they bind self for the
+;; RHS of their method-bodies or not. Otherwise, they are logically an
+;; ordered list of mappings from pattern to closure. Thus an object
+;; template has an environment template specifying what features of
+;; the lexical environment are to be closed over, and an object itself
+;; has an environment vector containing the closed-over values.
+;;
+;; We represent the ordered list using unordered data structures such
+;; as hash tables where doing so is indistinguishable from preserving
+;; the full ordering of the member clauses of the object.
+;;
+;; Note that objects can be constructed from smaller objects by
+;; application of the traits operators '/', '+', '-' and '@'.
