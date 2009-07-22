e7cc5cce2e376a74de94e6cdb842efc5c3c82885
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2009-07-22 20:30:03 +0100

    Make it compile on my OSX machine.

--- experiments/codegen/tinyscheme+cvs20090722/scheme.c
+++ experiments/codegen/tinyscheme+cvs20090722/scheme.c
@@ -4747,19 +4747,7 @@ pointer scheme_eval(scheme *sc, pointer obj)
 
 #if STANDALONE
 
-#if defined(__APPLE__) && !defined (OSX)
-int main()
-{
-     extern MacTS_main(int argc, char **argv);
-     char**    argv;
-     int argc = ccommand(&argv);
-     MacTS_main(argc,argv);
-     return 0;
-}
-int MacTS_main(int argc, char **argv) {
-#else
 int main(int argc, char **argv) {
-#endif
   scheme sc;
   FILE *fin;
   char *file_name=InitFile;
