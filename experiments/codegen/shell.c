38f7bbae855e87c87f9c7d19271a8d3bf6920187
Tony Garnock-Jones <tonyg@kcbbs.gen.nz>
2009-07-24 07:33:19 +0100

    Link in a disassembler to help with debugging

--- experiments/codegen/shell.c
+++ experiments/codegen/shell.c
@@ -7,6 +7,8 @@
 #include "scheme-private.h"
 #include "scheme.h"
 
+#include "libdis.h"
+
 static void die(char const *format, ...) {
   va_list vl;
   va_start(vl, format);
@@ -37,11 +39,46 @@ static pointer scm_build_native(scheme *sc, pointer args) {
   }
 }
 
+static pointer scm_disassemble(scheme *sc, pointer args) {
+  char line[128];
+  pointer s;
+  char *bytes;
+  int len;
+  int pos = 0;
+  x86_insn_t i;
+
+  if (!is_pair(args)) return sc->F;
+  s = pair_car(args);
+  if (!is_string(s)) return sc->F;
+
+  len = s->_object._string._length;
+  bytes = s->_object._string._svalue;
+
+  x86_init(opt_none, NULL, NULL);
+
+  while (pos < len) {
+    int i_size = x86_disasm((unsigned char *) bytes, len, 0, pos, &i);
+    if (i_size) {
+      x86_format_insn(&i, line, sizeof(line), att_syntax);
+      printf("%s\n", line);
+      pos += i_size;
+    } else {
+      printf("Invalid instruction\n");
+      pos++;
+    }
+  }
+
+  x86_cleanup();
+
+  return sc->T;
+}
+
 int main(int argc, char *argv[]) {
   scheme *sc;
   FILE *f;
   scheme_registerable fns[] = {
     { &scm_build_native, "build-native-function" },
+    { &scm_disassemble, "disassemble" },
   };
 
   sc = scheme_init_new();
