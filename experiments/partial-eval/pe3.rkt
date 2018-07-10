0d1eb951743f73ef40028ba90bc85fe95c5f6531
Tony Garnock-Jones <tonygarnockjones@gmail.com>
2018-07-11 00:04:08 +0100

    Repair pe3.rkt along lines suggested by pe3.hs

--- experiments/partial-eval/pe3.rkt
+++ experiments/partial-eval/pe3.rkt
@@ -12,7 +12,10 @@
 (define-syntax-rule (record N (F ...) extra ...)
   (struct N (F ...) #:transparent extra ...))
 
-;; Input AST
+;; PE translates an input AST plus an Environment into an AbsVal,
+;; while recording a sequential (!) *history* of named computations.
+
+;; AST
 (record Lit (value)) ;; (Lit RacketAtom)
 (record Prim (name handler)) ;; (Prim Symbol HandlerFun)
 (record Ref (id)) ;; (Ref Symbol)
@@ -22,62 +25,42 @@
 (record Bind (formal init body)) ;; (Bind Symbol AST AST)
 (record Letrec (formals inits body)) ;; (Letrec (Listof Symbol) (Listof AST) AST)
 
-;; ;; Names
-;; (record Impure (id))
-;; (record Pure (op args))
-;; (define *impure-counter* 0)
-;; (define (impure) (Impure (begin0 *impure-counter* (set! *impure-counter* (+ *impure-counter* 1)))))
-;; (define (pure op . args) (Pure op args))
-;; ;; Abstract values
-;; (record Value (name residual details))
+;; An AbsVal is an *abstract value*, one of
+;;  - (Unknown Symbol), for a completely unknown value whose
+;;    computation is named by Symbol in the history;
+;;  - (Runtime Symbol Description), for a partially- or
+;;    completely-known value whose computation is named by Symbol in the
+;;    history; or
+;;  - (Compiletime Description), for a completely-known value whose
+;;    computation does not and need not appear in the history.
+;;
+(record Unknown (reference))
+(record Runtime (reference description))
+(record Compiletime (description))
 
-;; PE translates an input AST plus an Environment into a sequence (!)
-;; of named computations, plus a final abstract-value.
-;;
-;; An abstract-value is a pair of
-;;  - a residual, which is either:
-;;      - a variable reference, if some run-time computation/allocation is involved, or
-;;      - #f, if the abstract-value is entirely compile-time;
-;;    and
-;;  - a description of everything known about the value produced by
-;;    the value the residual computation would yield.
-;;
 ;; An Environment maps user-level variable names to abstract-values.
-;;
 ;; Environment = (Listof (Cons Symbol AbsVal))
 
-(record Answer (computations value)) ;; (Answer (Listof (List Symbol AST AbsVal)) AbsVal)
-
-;; AbsVal
-(record Runtime (reference description)) ;; (Runtime Symbol (U Description Unknown))
-(record Compiletime (description)) ;; (Compiletime Description)
-
-;; Unknown
-(record Unknown ()) ;; (Unknown)
-
-;; Descriptions
+;; Description
 (record Atom (value)) ;; (Atom RacketAtom)
-;; Prim
+;; ... or Prim; or ...
 (record Pair (car cdr)) ;; (Pair AbsVal AbsVal)
 (record Closure (formals body env)) ;; (Closure (Listof Symbol) AST Environment)
 
-(define (next-id base) (gensym base))
+;;---------------------------------------------------------------------------
 
-(define (known-case v k-known k-unknown)
-  (match v
-    [(Runtime r (Unknown)) (k-unknown r)]
-    [(Runtime r d) (k-known r d)]
-    [(Compiletime d) (k-known #f d)]))
+(define-match-expander Known
+  (syntax-rules ()
+    [(_ desc)
+     (or (Runtime _ desc)
+         (Compiletime desc))]))
 
 (define (known? v)
   (or (Compiletime? v)
-      (and (Runtime? v)
-           (not (Unknown? (Runtime-description v))))))
+      (Runtime? v)))
 
 (define (known-value v)
-  (match v
-    [(Compiletime d) d]
-    [(Runtime _ d) d]))
+  (match v [(Known d) d]))
 
 (define (unatom v)
   (Atom-value (known-value v)))
@@ -144,61 +127,58 @@
 
 (define *globals* (make-hash))
 
-;; (define (extend-env/undefined env names)
-;;   (append (for/list [(name names)] (cons name (box #f))) env))
-
 (define (extend-env env names inits)
   (append (for/list [(name names) (init inits)] (cons name (box init))) env))
 
-(define (extend-env/computations env cs)
-  (append (for/list [(c cs)]
-            (match-define (list id ast av) c)
-            (cons id (box av)))
-          env))
-
 (define (lookup-env env name)
   (match (assq name env)
-    [#f (unbox (hash-ref *globals*
-                         name
-                         (lambda () (error 'lookup-env "Unbound variable: ~v" name))))]
-    [(cons _name (box absval)) absval]))
+    [(cons _name (box absval)) absval]
+    [#f (let loop ((hs (pe-history)))
+          (match hs
+            ['()
+             (unbox (hash-ref *globals*
+                              name
+                              (lambda () (error 'lookup-env "Unbound variable: ~v" name))))]
+            [(cons h hs)
+             (match (assq name h)
+               [(list _name _ast absval) absval]
+               [#f (loop hs)])]))]))
 
 ;;---------------------------------------------------------------------------
 
-(define (pe-step pexp env k)
-  (match (pe pexp env)
-    [(Answer cs1 absval)
-     (match (k absval (extend-env/computations env cs1))
-       [(Answer cs2 final-absval)
-        (D `(gluing ,cs1 ,cs2))
-        (Answer (append cs1 cs2) final-absval)])]))
+;; pe-history : (Parameterof History)
+;; History = (Listof Era)
+;; Era = (Listof (List Symbol AST AbsVal))
+(define pe-history (make-parameter '()))
 
-(define (return absval)
-  (Answer '() absval))
+(define (next-id base) (gensym base))
+
+(define-syntax-rule (residualize expr)
+  (parameterize ((pe-history (cons '() (pe-history))))
+    (define av expr)
+    (wrap-era (car (pe-history)) (codegen-absval av))))
+
+(define (wrap-era h body)
+  (match h
+    ['() body]
+    [(cons (list id ast av) h)
+     (if (equal? body (Ref id))
+         (wrap-era h ast)
+         (wrap-era h (Bind id ast body)))]))
 
 (define-syntax-rule (emit [id ast-expr] av-expr)
-  (let* ((ast ast-expr)
-         (id (next-id 'id))
+  (let* ((id (next-id 'id))
+         (ast ast-expr)
          (av av-expr))
-    (D `(emitting ,id ,ast ,av))
-    (Answer (list (list id ast av)) av)))
-
-(define (codegen ans)
-  (match-define (Answer defs absval) ans)
-  (let loop ((defs defs))
-    (match defs
-      ['() (codegen-absval absval)]
-      [(cons (list id ast av) defs)
-       (Bind id ast (loop defs))])))
+    (match-define (cons h hs) (pe-history))
+    (pe-history (cons (cons (list id ast av) h) hs))
+    av))
 
 (define (codegen-absval absval)
-  (known-case absval
-              (lambda (id d)
-                (if id
-                    (Ref id)
-                    (codegen-desc d)))
-              (lambda (id)
-                (Ref id))))
+  (match absval
+    [(Unknown id) (Ref id)]
+    [(Runtime id _) (Ref id)]
+    [(Compiletime d) (codegen-desc d)]))
 
 (define (codegen-desc d)
   (match d
@@ -209,82 +189,56 @@
 
 (define INDENT (make-parameter 0))
 (define (D x)
-  (when #t
+  (when #f ;; #t
     (display (make-string (INDENT) #\space))
     (display x)
     (newline)))
 
-;; AST Environment -> Answer
+;; AST Environment -> AbsVal
 (define (pe pexp env)
   (D `((pexp ,pexp) (env ,env)))
   (parameterize ((INDENT (+ (INDENT) 2)))
   (match pexp
-    [(Lit v) (return (Compiletime (Atom v)))]
-
-    [(Prim _ _) (return (Compiletime pexp))]
-
-    [(Ref id) (return (lookup-env env id))]
+    [(Lit v) (Compiletime (Atom v))]
+    [(Prim _ _) (Compiletime pexp)]
+    [(Ref id) (lookup-env env id)]
 
     [(If test true false)
-     (pe-step test env
-              (lambda (test-v env)
-                (known-case test-v
-                            (lambda (test-id test-d)
-                              (if (equal? test-d (Atom #f))
-                                  (pe false env)
-                                  (pe true env)))
-                            (lambda (test-id)
-                              (emit [if-id (If (Ref test-id)
-                                               (codegen (pe true env))
-                                               (codegen (pe false env)))]
-                                    (Runtime if-id (Unknown)))))))]
+     (match (pe test env)
+       [(Known (Atom #f)) (pe false env)]
+       [(Known _) (pe true env)]
+       [(Unknown test-id)
+        (emit [if-id (If (Ref test-id)
+                         (residualize (pe true env))
+                         (residualize (pe false env)))]
+              (Unknown if-id))])]
 
     [(Lambda formals body)
-     (define cloenv
-       (let ((free (filter (lambda (i) (assq i env)) ;; only the non-global names
-                           (set->list (free-names pexp)))))
-         (extend-env '()
-                     free
-                     (for/list [(f free)] (lookup-env env f)))))
-     (define clo (Closure formals
-                          (codegen (pe body
-                                       (extend-env cloenv
-                                                   formals
-                                                   (for/list [(formal formals)]
-                                                     (Runtime formal (Unknown))))))
-                          cloenv))
+     (define (non-global? id) (assq id env))
+     (define captured (filter non-global? (set->list (free-names pexp))))
+     (define cloenv (extend-env '() captured (for/list [(c captured)] (lookup-env env c))))
+     (define new-body (residualize (pe body (extend-env cloenv formals (map Unknown formals)))))
+     (define clo (Closure formals new-body cloenv))
      (emit [lam-id (codegen-desc clo)]
            (Runtime lam-id clo))]
 
     [(Apply rator rands)
-     (pe-step rator env
-              (lambda (rator-v env)
-                (let loop ((env env) (rands rands) (rands-vs-rev '()))
-                  (match rands
-                    ['()
-                     (define rands-vs (reverse rands-vs-rev))
-                     (known-case rator-v
-                                 (lambda (rator-id d)
-                                   (match d
-                                     [(Closure formals body cloenv)
-                                      (D `(--> closure ,body))
-                                      (pe body (extend-env cloenv formals rands-vs))]
-                                     [(Prim _name handler)
-                                      (D `(--> prim ,_name ,@rands-vs))
-                                      (apply handler d rands-vs)]))
-                                 (lambda (rator-id)
-                                   (emit [app-id (Apply (Ref rator-id)
-                                                        (map codegen-absval rands-vs))]
-                                         (Runtime app-id (Unknown)))))]
-                    [(cons rand rands)
-                     (pe-step rand env
-                              (lambda (rand-v env)
-                                (loop env rands (cons rand-v rands-vs-rev))))]))))]
+     (define rator-v (pe rator env))
+     (define rands-vs (for/list [(rand rands)] (pe rand env)))
+     (match rator-v
+       [(Known (Closure formals body cloenv))
+        (D `(--> closure ,body))
+        (pe body (extend-env cloenv formals rands-vs))]
+       [(Known (and prim (Prim _name handler)))
+        (D `(--> prim ,_name ,@rands-vs))
+        (apply handler prim rands-vs)]
+       [(Unknown rator-id)
+        (emit [app-id (Apply (Ref rator-id) (map codegen-absval rands-vs))]
+              (Unknown app-id))])]
 
     [(Bind formal init body)
-     (pe-step init env
-              (lambda (init-v env)
-                (pe body (extend-env env (list formal) (list init-v)))))]
+     (define init-v (pe init env))
+     (pe body (extend-env env (list formal) (list init-v)))]
 
     [(Letrec formals inits body)
      (error 'pe "Unimplemented: Letrec")])
@@ -294,18 +248,18 @@
 
 (define (prim-app prim . args)
   (emit [app-id (Apply prim (map codegen-absval args))]
-        (Runtime app-id (Unknown))))
+        (Unknown app-id)))
 
 (define (lift-residualize f)
   (lambda (self . args)
     (if (andmap known? args)
-        (return (Compiletime (Atom (apply f (map unatom args)))))
+        (Compiletime (Atom (apply f (map unatom args))))
         (apply prim-app self args))))
 
 (define (lift-residualize* f)
   (lambda (self . args)
     (if (andmap known? args)
-        (return (Compiletime (Atom (apply f args))))
+        (Compiletime (Atom (apply f args)))
         (apply prim-app self args))))
 
 (define (lift-commutative-associative-binop f identity)
@@ -313,7 +267,7 @@
     (define-values (known unknown) (partition known? vals))
     (define part-val (apply f (map unatom known)))
     (define part (Compiletime (Atom part-val)))
-    (cond [(null? unknown) (return part)]
+    (cond [(null? unknown) part]
           [(= identity part-val) (apply prim-app self unknown)]
           [else (apply prim-app self part unknown)])))
 
@@ -343,13 +297,13 @@
            ;;                  (prim-app 'eq? x y))))
 
            (Prim 'PRIMcar (lambda (self x)
-                            (if (known? x)
-                                (return (Pair-car (known-value x)))
-                                (prim-app self x))))
+                            (match x
+                              [(Known (Pair a _)) a]
+                              [_ (prim-app self x)])))
            (Prim 'PRIMcdr (lambda (self x)
-                            (if (known? x)
-                                (return (Pair-cdr (known-value x)))
-                                (prim-app self x))))
+                            (match x
+                              [(Known (Pair _ d)) d]
+                              [_ (prim-app self x)])))
            ))
 
 ;;---------------------------------------------------------------------------
@@ -364,44 +318,33 @@
        (compose (lambda (a) (* a 2))
                 (lambda (b) (+ b 1)))))
 
-  (define (reconstruct-ast v)
+  (define (reconstruct v)
     (match v
-      ;; [(Closure formals body '())
-      ;;  `(lambda ,formals ,(reconstruct-ast body))]
-      ;; [(Closure formals body env)
-      ;;  `(let (,@(map (lambda (entry)
-      ;;                  (match-define (cons name id) entry)
-      ;;                  (list name id))
-      ;;                env))
-      ;;     (lambda ,formals ,(reconstruct-ast body)))]
       [(Lit v)
        `(quote ,v)]
       [(Prim name _)
-       `(PRIM ,name)]
+       (string->symbol (string-append "#%" (symbol->string name)))]
       [(Ref id)
        id]
       [(If test true false)
-       `(if ,(reconstruct-ast test) ,(reconstruct-ast true) ,(reconstruct-ast false))]
+       `(if ,(reconstruct test) ,(reconstruct true) ,(reconstruct false))]
       [(Lambda formals body)
-       `(lambda ,formals ,(reconstruct-ast body))]
+       `(lambda ,formals ,(reconstruct body))]
       [(Apply rator rands)
-       `(,(reconstruct-ast rator) ,@(map reconstruct-ast rands))]
+       `(,(reconstruct rator) ,@(map reconstruct rands))]
       [(Bind formal init body)
-       `(let ((,formal ,(reconstruct-ast init))) ,(reconstruct-ast body))]
+       (reconstruct-binds (list (list formal (reconstruct init))) body)]
       ))
 
-  (define (reconstruct ans)
-    (match-define (Answer history absval) ans)
-    `(let* (,@(map (lambda (entry)
-                     (match-define (list id ast av) entry)
-                     (list id (reconstruct-ast ast)))
-                   history))
-       ,(reconstruct-ast (codegen-absval absval))))
+  (define (reconstruct-binds bs body)
+    (match body
+      [(Bind formal init body) (reconstruct-binds (cons (list formal (reconstruct init)) bs) body)]
+      [_ `(let* ,(reverse bs) ,(reconstruct body))]))
 
   (define (T e)
-    (define ans (pe (parse e) '()))
-    ;; (pretty-display ans)
-    (pretty-display (reconstruct ans)))
+    (define ast (residualize (time (pe (parse e) '()))))
+    ;; (pretty-display ast)
+    (pretty-display (reconstruct ast)))
 
   ;; (T '((lambda (x) (+ x 1)) 123))
   ;; (T `(let ((c (lambda (f) (f 123))))
@@ -409,7 +352,17 @@
 
   ;; (T `(let ((p (cons 1 2))) (PRIMcdr (PRIMcar (cons p p)))))
 
-  ;; (T compose-exp)
+  (T compose-exp)
+
+  ;; (T
+  ;;  '(lambda (do-something-with bb)
+  ;;     ((((lambda (a)
+  ;;          (lambda (b)
+  ;;            (lambda (c)
+  ;;              (do-something-with a b c))))
+  ;;        'aa)
+  ;;       (bb))
+  ;;      'cc)))
 
   ;; (T
   ;;  '(lambda (do-something-with bb)
@@ -419,9 +372,9 @@
   ;;                       (bb)))
   ;;      'aa)))
 
-  (T
-   '(lambda (bb)
-      ((lambda (k b) (k (lambda () b)))
-       (lambda (f) (f))
-       (bb))))
+  ;; (T
+  ;;  '(lambda (bb)
+  ;;     ((lambda (k b) (k (lambda () b)))
+  ;;      (lambda (f) (f))
+  ;;      (bb))))
   )
